d7c7a972f1d879202c111518816d3c3b382ebdab
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2022-11-07 05:37:20 +0000

    [Add]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
   get '/top' => 'homes#top'
-
 end

3b1fdecce9ec784bd08263f26139f5aec9de5daf
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2022-11-09 06:45:23 +0000

    [Add]4-8バリデーション設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,12 +7,15 @@ class ListsController < ApplicationController
   # 以下を追加
   def create
     # 1.&2. データを受け取り新規作成するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    if @list.save
     # redirect_to '/top' を削除して、以下コードに変更
     # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
